66a3ebb253eca14281592544c6e56d33de324ef8
Michael Nusair‬‏ <mnpcmw6444@gmail.com>
2023-08-29 22:41:02 +0300

    server

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,21 @@
-import express from 'express';
-import http from 'http';
-import socketIO from 'socket.io';
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
+const httpServer = createServer();
+const io = new Server(httpServer);
 
-io.on('connection', (socket) => {
-    console.log('A user connected');
+io.on("connection", (socket) => {
+    console.log("A user connected");
 
-    // Example: Notify all clients about a new message
-    socket.on('new-message', (message) => {
-        io.emit('new-message', message);
+    socket.on("new-message", (message) => {
+        io.emit("new-message", message);
     });
 
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
     });
 });
 
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
+httpServer.listen(3000, () => {
+    console.log("Server is running on port 3000");
 });
